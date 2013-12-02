2ccc77ff3d5241679f99644ebcc3f355999f3caa
Celeo <celeodor@gmail.com>
2013-12-01 19:09:06 -0800

    Conversion to multi-world complete

--- zerg/views.py
+++ zerg/views.py
@@ -4,65 +4,6 @@ import urllib2
 import json
 
 # ===============================
-#        Data setup
-# ===============================
-
-troll_event = 'D17D47E9-0A87-4189-B02A-54E23AA91A82'
-boar_event = '69D031A8-7AD2-4419-B564-48457841A57C'
-oak_event = '04084490-0117-4D56-8D67-C4FFFE933C0C'
-bandit_event = 'BC997F15-4C05-4D95-A14F-9B7C4CF41B4E'
-wasp_event = '3C3915FB-E2E4-4794-A700-E3B5FCFE0404'
-sb_event = '31CEBA08-E44D-472F-81B0-7143D73797F5'
-zerg_events = [troll_event, boar_event, oak_event, bandit_event, wasp_event, sb_event]
-
-worlds = {}
-for d in json.loads(urllib2.urlopen('https://api.guildwars2.com/v1/world_names.json?lang=en').read()):
-    worlds[d['id']] = d['name']
-world_names = [str(d).replace(' ', '_') for d in worlds.itervalues()]
-
-class Champion:
-    def __init__(self, name='', event_id='', status='Unknown'):
-        self.name = name
-        self.event_id = event_id
-        self.status = status
-    def __repr__(self):
-        return '%s: %s' % (self.name, self.status)
-    def update_status(self, new_status):
-        self.status = new_status
-    def is_active(self):
-        return self.status == 'Active'
-
-class World:
-    def __init__(self, world_id, world_name, troll, boar, oak, bandit, wasp, shadow):
-        self.id = world_id
-        self.name = world_name
-        self.troll = troll
-        self.boar = boar
-        self.oak = oak
-        self.bandit = bandit
-        self.wasp = wasp
-        self.shadow = shadow
-        self.last_updated = datetime.now()
-    def get_champion_status(self, champion_name):
-        if champion_name == 'troll':
-            return self.troll.status
-        elif champion_name == 'boar':
-            return self.boar.status
-        elif champion_name == 'oak':
-            return self.oak.status
-        elif champion_name == 'bandit':
-            return self.bandit.status
-        elif champion_name == 'wasp':
-            return self.wasp.status
-        elif champion_name == 'shadow':
-            return self.shadow.status
-    def try_update(self):
-        if not last_updated or (datetime.now() - self.last_updated).seconds > 30:
-            self._update()
-    def _update(self):
-        pass
-
-# ===============================
 #       Data use
 # ===============================
 
@@ -80,25 +21,96 @@ def get_short_name(event_id):
     elif event_id == sb_event:
         return 'SB'
 
-def should_update():
-    global last_updated
-    if last_updated == None:
-        return True
-    now = datetime.now()
-    diff = now - last_updated
-    return diff.seconds > 30
-
-def get_all_current_events(world_id):
-    ret = [x for x in json.loads(urllib2.urlopen('https://api.guildwars2.com/v1/events.json?world_id=%s&map_id=15&lang=en' % world_id).read())['events']]
-    return ret
-
 def get_world_object(world_id):
     """ Retruns World class object matching the world_id """
+    for world in worlds:
+        if world.world_id == world_id:
+            return world
     return None
 
 def is_zerg_event(event_id):
     """ Returns True if event_id matches that of a zerg event """ 
     return event_id in zerg_events
+
+# ===============================
+#        Classes
+# ===============================
+
+class Champion:
+    def __init__(self, name, event_id, status='Unknown'):
+        self.name = name
+        self.event_id = event_id
+        self.status = status
+    def __repr__(self):
+        return '<Champion %s: %s>' % (self.name, self.status)
+    def is_active(self):
+        return self.status == 'Active'
+
+class World:
+    def __init__(self, world_id, world_name):
+        self.world_id = world_id
+        self.world_name = world_name
+        self.troll = Champion(name='Troll', event_id=troll_event)
+        self.boar = Champion(name='Boar', event_id=boar_event)
+        self.oak = Champion(name='Oak', event_id=oak_event)
+        self.bandit = Champion(name='Bandit', event_id=bandit_event)
+        self.wasp = Champion(name='Wasp', event_id=wasp_event)
+        self.shadow = Champion(name='Shadow', event_id=sb_event)
+        self.last_updated = None
+    def __repr__(self):
+        return '<World %s %s>' % (self.world_id, self.world_name)
+    def get_champion_status(self, champion_name):
+        if champion_name == 'troll':
+            return self.troll.status
+        elif champion_name == 'boar':
+            return self.boar.status
+        elif champion_name == 'oak':
+            return self.oak.status
+        elif champion_name == 'bandit':
+            return self.bandit.status
+        elif champion_name == 'wasp':
+            return self.wasp.status
+        elif champion_name == 'shadow':
+            return self.shadow.status
+    def try_update(self):
+        if not self.last_updated or (datetime.now() - self.last_updated).seconds > 30:
+            self._update()
+    def _update(self):
+        events = [x for x in json.loads(urllib2.urlopen('https://api.guildwars2.com/v1/events.json?world_id=%s&map_id=15&lang=en' % self.world_id).read())['events']]
+        for event in events:
+            if not is_zerg_event(event['event_id']):
+                continue
+            n = get_short_name(event['event_id'])
+            if n == 'Troll':
+                self.troll.status = event['state']
+            elif n == 'Boar':
+                self.boar.status = event['state']
+            elif n == 'Oak':
+                self.oak.status = event['state']
+            elif n == 'Bandit':
+                self.bandit.status = event['state']
+            elif n == 'Wasp':
+                self.wasp.status = event['state']
+            elif n == 'SB':
+                self.shadow.status = event['state']
+        self.last_updated = datetime.now()
+
+# ===============================
+#        Data setup
+# ===============================
+
+troll_event = 'D17D47E9-0A87-4189-B02A-54E23AA91A82'
+boar_event = '69D031A8-7AD2-4419-B564-48457841A57C'
+oak_event = '04084490-0117-4D56-8D67-C4FFFE933C0C'
+bandit_event = 'BC997F15-4C05-4D95-A14F-9B7C4CF41B4E'
+wasp_event = '3C3915FB-E2E4-4794-A700-E3B5FCFE0404'
+sb_event = '31CEBA08-E44D-472F-81B0-7143D73797F5'
+zerg_events = [troll_event, boar_event, oak_event, bandit_event, wasp_event, sb_event]
+
+worlds = []
+for d in json.loads(urllib2.urlopen('https://api.guildwars2.com/v1/world_names.json?lang=en').read()):
+    worlds.append(World(world_id=d['id'], world_name=d['name']))
+world_names = [world.world_name.replace(' ', '_') for world in worlds]
 
 # ===============================
 #           Pages
@@ -112,13 +124,13 @@ def world(request, world_name):
     """ Base page for this world - load and continue to refresh that world's data """
     world_name = world_name.replace('_', ' ')
     world_id = -1
-    for w_id, name in worlds.items():
-        if name == world_name:
-            world_id = w_id
-    return render(request, 'zerg/index.html', {'world_name': world_name, 'world_id': world_id})
+    for world in worlds:
+        if world.world_name == world_name:
+            world_id = world.world_id
+    return render(request, 'zerg/world.html', {'world_name': world_name, 'world_id': world_id})
 
 def data(request, world_id):
     """ Data page for loading via Javacsript - return render of all data for that world """
     world = get_world_object(world_id)
     world.try_update()
-    return render(request, 'zerg/data.html', {'last_updated': last_updated, 'now': datetime.now(), 'world': world})
+    return render(request, 'zerg/data.html', {'now': datetime.now(), 'world': world})
